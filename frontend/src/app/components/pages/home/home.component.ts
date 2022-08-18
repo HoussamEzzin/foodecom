fe7b5d5e07090d5f186ceb186993eeafc053ae96
Ezzin Houssam <ezzinhoussam@gmail.com>
2022-08-19 00:19:26 +0100

    UI dev

--- frontend/src/app/components/pages/home/home.component.ts
+++ frontend/src/app/components/pages/home/home.component.ts
@@ -1,6 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { FoodService } from 'src/app/services/food.service';
 import { Food } from 'src/app/shared/models/Food';
+import {ActivatedRoute} from "@angular/router";
 
 @Component({
   selector: 'app-home',
@@ -10,8 +11,15 @@ import { Food } from 'src/app/shared/models/Food';
 export class HomeComponent implements OnInit {
 
   foods:Food[] =[];
-  constructor(private foodService:FoodService) {
-    this.foods = foodService.getAll();
+  constructor(private foodService:FoodService, activatedRoute:ActivatedRoute) {
+    activatedRoute.params.subscribe((params)=>{
+      if(params['searchTerm'])
+        this.foods = this.foodService.getAllFoodsBySearchTerm(params['searchTerm']);
+      else if(params["tag"])
+        this.foods = this.foodService.getAllFoodsByTag(params['tag']);
+      else
+        this.foods = foodService.getAll();
+    })
    }
 
   ngOnInit(): void {
--- frontend/src/app/services/food.service.ts
+++ frontend/src/app/services/food.service.ts
@@ -1,6 +1,7 @@
 import { Injectable } from '@angular/core';
-import { sample_foods } from 'src/data';
+import {sample_foods, sample_tags} from 'src/data';
 import { Food } from '../shared/models/Food';
+import {Tag} from "../shared/models/Tag";
 
 @Injectable({
   providedIn: 'root'
@@ -12,4 +13,35 @@ export class FoodService {
   getAll(): Food[]{
     return sample_foods;
   }
+
+  getAllFoodsBySearchTerm(searchTerm:string){
+
+    if(searchTerm ==""){
+      return this.getAll()
+    }
+
+    return this.getAll().filter(food => food.name.toLowerCase().includes(searchTerm.toLowerCase()))
+
+  }
+
+  getAllTags(): Tag[]{
+    return sample_tags;
+  }
+
+  getAllFoodsByTag(tag:string): Food[]{
+    return tag == "All" ?
+    this.getAll() :
+    this.getAll().filter(food => food.tags?.includes(tag))
+  }
+
+  getFoodById(foodId:string):Food{
+    //The nullish coalescing operator (??) is a logical operator that
+    // returns its right-hand side operand when
+    // its left-hand side operand is null or undefined,
+    // and otherwise returns its left-hand side operand
+    return this.getAll().find(food => food.id == foodId) ?? new Food();
+  }
+
+
+
 }
